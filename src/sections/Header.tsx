4e3c2898eded5cc2ef603fb6fc413c6d0804369b
devotedayush <23f1001477@ds.study.iitm.ac.in>
2025-02-05 23:43:13 +0530

    updates

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return <div>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Join Us</a>
+      <a href="#">Contact Us</a>
+      <a href="#">Sign Up</a>
+    </nav>
+  </div>;
 };

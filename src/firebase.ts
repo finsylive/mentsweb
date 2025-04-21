e22671d0a70e8bfe2c2558ed5afb58c4179fc5a6
Rajkamal45 <24f1000149@ds.study.iitm.ac.in>
2025-04-21 19:00:25 +0530

    firebase setting

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,22 +1,27 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebase.ts
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getAnalytics, Analytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDTXuTUyy1P809-cxez8veNjvXNWtDak5Q",
+  apiKey:    "AIzaSyDTXuTUyy1P809-cxez8veNjvXNWtDak5Q",
   authDomain: "ments-web-994dd.firebaseapp.com",
-  projectId: "ments-web-994dd",
+  projectId:  "ments-web-994dd",
   storageBucket: "ments-web-994dd.firebasestorage.app",
   messagingSenderId: "712478577069",
-  appId: "1:712478577069:web:a50debb8cff1ae9ef7d124",
-  measurementId: "G-MQ3K1YYLHV"
+  appId:     "1:712478577069:web:a50debb8cff1ae9ef7d124",
+  measurementId: "G-MQ3K1YYLHV",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase App
+const app: FirebaseApp = initializeApp(firebaseConfig);
+console.log("Firebase App Initialized:", app.name);  // should log "[DEFAULT]"
+
+// Initialize Analytics (only in the browser)
+let analytics: Analytics | undefined;
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+  console.log("Analytics Initialized:", analytics);
+}
+
 export { app, analytics };
